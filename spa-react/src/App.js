3f899dee1c28c4286f12298c46b060f1e9b0afdb
Phyo Zaw Linn <phyozawlinn1852020@gmail.com>
2024-12-27 19:40:10 +0630

    another commit

--- spa-react/src/App.js
+++ spa-react/src/App.js
@@ -7,6 +7,21 @@ function App() {
 
   let [name, setName] = useState("Phyo Zaw Linn"); //useState return an array [getter,setterFun]
 
+  let [post] = useState([
+    {
+      id : 1,
+      title : "first post"
+    },
+    {
+      id : 2,
+      title : "second post"
+    },
+    {
+      id : 3,
+      title : "third post"
+    }
+  ])
+
   let changeName = () => {
     // name = "Linn Zaw Phyo";
     setName("Linn Zaw Phyo") // This is the async function
@@ -17,6 +32,14 @@ function App() {
     <div className="App">
       <h2>Hello {name}</h2>
       <button onClick={changeName}>Change name</button>
+      <h3>Posts</h3>
+
+      <ul>
+        {post.map((post)=>(
+          <li key={post.id}>{post.title}</li>
+        ))}
+      </ul>
+
     </div>
   );
 }

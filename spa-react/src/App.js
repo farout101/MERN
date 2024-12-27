0901dad1d8343f0680306685126939fffd3e8031
Phyo Zaw Linn <phyozawlinn1852020@gmail.com>
2024-12-27 12:39:12 +0630

    another test

--- spa-react/src/App.js
+++ spa-react/src/App.js
@@ -1,9 +1,22 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  // let name = "Phyo Zaw Linn"
+
+  let [name, setName] = useState("Phyo Zaw Linn"); //useState return an array [getter,setterFun]
+
+  let changeName = () => {
+    // name = "Linn Zaw Phyo";
+    setName("Linn Zaw Phyo") // This is the async function
+    // console.log(name)
+  }
+  
   return (
     <div className="App">
-      <h2>Hell yeah</h2>
+      <h2>Hello {name}</h2>
+      <button onClick={changeName}>Change name</button>
     </div>
   );
 }

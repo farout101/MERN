00d892ad6e2c22fb73168c2e607cabefbe1eb46e
Phyo Zaw Linn <phyozawlinn1852020@gmail.com>
2024-12-28 13:23:49 +0630

    css module system

--- spa-react/src/components/postlist/index.js
+++ spa-react/src/components/postlist/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import './index.css'
+import styles from './single_post.module.css'
 
 export default function Postlist({posts}) {
 
@@ -7,7 +8,7 @@ export default function Postlist({posts}) {
     <div className='postlist-component'>
       <div className='postList container'>
         {posts.map(post=> (
-          <div className='single-post' key={post.id}>{post.title}</div>
+          <div className={`single-post ${styles.card}`} key={post.id}>{post.title}</div>
         ))}
       </div>
     </div>

01a72f34bd9edcf35989b31256e904cb7239eb40
Phyo Zaw Linn <phyozawlinn1852020@gmail.com>
2024-12-30 11:38:12 +0630

    ep 28

--- spa-react/src/components/PostForm/index.js
+++ spa-react/src/components/PostForm/index.js
@@ -1,24 +1,28 @@
-import React, { useState } from 'react'
-import './index.css'
+import React, { useRef } from 'react';
+import './index.css';
 
 export default function PostForm({addPost}) { //the function name can't be index anyway
   
-    let [title,setTitle] = useState("")
+    let refTitle = useRef();
 
-    let onChangeHandler = (e) => {
-        setTitle(e.target.value)
-    }
+    // let [title,setTitle] = useState("")
+
+    // let onChangeHandler = (e) => {
+    //     setTitle(e.target.value)
+    // }
 
     let resetForm = () => {
-        setTitle("")
+        refTitle.current.value = ""
     }
 
     let uploadPost = (e) => { // refreshing is the html default nature and there's nothing to do with react
         e.preventDefault() // prevent refresh page
 
+        // console.log(refTitle.current.value)
+
         let post = {
             id : Math.floor(Math.random()*10000),
-            title : title
+            title : refTitle.current.value
         }
 
         console.log(post)
@@ -31,11 +35,9 @@ export default function PostForm({addPost}) { //the function name can't be index
         <h1>Create Post</h1>
         <div className='form-control'>
             <label htmlFor=''>Title</label>
-            <input type="text" onChange={onChangeHandler} value={title}/>
+            <input type="text" ref={refTitle} />
         </div>
-        {!!title.length}
-        {title ? <p>{title}</p> : <p>Nothing is here</p>}
-        <button type="button" onClick={resetForm}>rest form</button>
+        <button type="button" onClick={resetForm}>Reset Form</button>
         <div className='form-control'>
             <button type='submit'>Post Now</button>
         </div>

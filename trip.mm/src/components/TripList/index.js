e55044b43fb3a7d97222ae69c6e23522f9b73630
Hacker Mg Pyuue <phyozawlinn1852020@gmail.com>
2025-01-04 13:55:57 +0630

    another commit

--- trip.mm/src/components/TripList/index.js
+++ trip.mm/src/components/TripList/index.js
@@ -1,34 +1,48 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
+import useFetch from '../../hooks/useFetch'
+import './index.css'
 
 export default function Index() {
 
-    let [trips, setTrips] = useState([])
+    // let [trips,setTrips] = useState([])
 
     let [url,setUrl] = useState('http://localhost:3000/trips/')
 
-    useEffect(()=>{
-        fetch(url)
-        .then(res => res.json())
-        .then(data => {
-            setTrips (data)
-        })
-    },[url])
+    // let fetchTrips = useCallback(() => { //Stores a function as cache
+    //     fetch(url)
+    //     .then(res => res.json())
+    //     .then(data => {
+    //         setTrips (data)
+    //     })
+    // },[url])
+
+    let { data : trips, loading, error } = useFetch(url)
 
     return (
-        <div>
-            <h1>Ready to Go?</h1>
+        <div className='container'>
+            <div className='flex-container'>
+                {error && <p>{error}</p>}
 
-            <button onClick={() => setUrl('http://localhost:3000/trips/')}>All</button>
-            <button onClick={() => setUrl('http://localhost:3000/trips/?location=Myanmar')}>Filter by Myanmar</button>
+                {!error && <div>
+                    <h1>Ready to Go?</h1>
+                
+                    <div>
+                        <button onClick={() => setUrl('http://localhost:3000/trips/')}>All</button>
+                        <button onClick={() => setUrl('http://localhost:3000/trips/?location=Myanmar')}>Filter by Myanmar</button>
+                    </div>
 
-            <ul>
-                {trips.map(trip => (
-                <li key={trip.id}>
-                    <h3>{trip.name}</h3>
-                    <p>price - {trip.price}</p>
-                </li>
-                ))}
-            </ul>
+                    {loading && <p>Lists are still Loading</p>}
+
+                    <ul className='trips-list'>
+                        {trips && trips.map(trip => ( // Data fetching takes time so we need a placeholder or wait it until it's done
+                        <li className='trip' key={trip.id}>
+                            <h3>{trip.name}</h3>
+                            <p>price - {trip.price}</p>
+                        </li>
+                        ))}
+                    </ul>     
+                </div>}
+            </div>
         </div>
     )
 }

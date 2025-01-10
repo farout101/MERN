8828f43488f83df157f14a2f5115f2aef9020a82
Phyo Zaw Linn <phyozawlinn1852020@gmail.com>
2025-01-10 21:41:22 +0630

    changing laptop

--- react-rounting/src/index.js
+++ react-rounting/src/index.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import {
+    createBrowserRouter,
+    RouterProvider
+} from "react-router-dom";
 import App from './App';
+import './index.css';
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App/>
+    }
+])
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App />
+    <RouterProvider router={router}/>
 );

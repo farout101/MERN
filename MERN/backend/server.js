481eb41092ba65e9a5bdba9388a2cba86aa2f42d
Hacker Mg Pyuue <phyozawlinn1852020@gmail.com>
2025-01-14 22:02:00 +0630

    mongo connection

--- MERN/backend/server.js
+++ MERN/backend/server.js
@@ -14,14 +14,24 @@ const recipeRoutes = require('./routes/recipes')
 // define the express app
 const app = express()
 
+// importing Mongoose
+const mongoose = require('mongoose')
+
+// mongoDB url
+const mongoURL = "mongodb+srv://farout101:test1234@mern-cluster.b8rcf.mongodb.net/?retryWrites=true&w=majority&appName=MERN-Cluster"
+mongoose.connect(mongoURL).then(() => {
+    console.log('connected to db...')
+
+    // The PORT number came from the .env file with the help of dotenv package
+    // This code came only after the database import
+    app.listen(process.env.PORT, () => { 
+    console.log("Express is running on local host "+ process.env.PORT)
+    })
+})
+
 // we'll use the dev version of morgan
 app.use(morgan('dev'))
 
 // Use the routes
 // We just have to call use because the processing is happening inside the other folder
 app.use('/api/recipes',recipeRoutes)
-
-// The PORT number came from the .env file with the help of dotenv package
-app.listen(process.env.PORT, () => { 
-    console.log("Express is running on local host "+ process.env.PORT)
-})

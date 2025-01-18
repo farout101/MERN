cd8b0121523576a683da6d1e3a9161129232cc09
Phyo Zaw Linn <phyozawlinn1852020@gmail.com>
2025-01-18 18:21:28 +0630

    Refactor useFetch hook to remove redundant code and improve fetch handling

--- react-rounting/src/hooks/useFetch.js
+++ react-rounting/src/hooks/useFetch.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { useEffect, useState } from "react"; // Importing useEffect and useState hooks from React
 
 function useFetch(url) {
@@ -8,13 +7,10 @@ function useFetch(url) {
 
     useEffect(() => {
         let abortController = new AbortController(); // Create an AbortController to handle fetch cancellation
-        let signal = abortController.signal; // Get the signal from the AbortController
 
         setLoading(true); // Set loading state to true before starting the fetch
 
-        
-        
-        // Perform the fetch request with the provided URL and signal
+        fetch(url, { signal: abortController.signal }) // Perform the fetch request with the provided URL and signal
             .then(res => {
                 if (!res.ok) { // Check if the response is not ok (status code not in the range 200-299)
                     throw Error('something went wrong'); // Throw an error if response is not ok
@@ -41,44 +37,3 @@ function useFetch(url) {
 }
 
 export default useFetch; // Export the custom hook
-=======
-import { useEffect, useState } from "react";
-
-function useFetch(url) {
-    let [data,setData] = useState(null);
-    let [ loading, setLoading ] = useState(false);
-    let [ error, setError ] = useState(null);
-
-    useEffect(() => {
-      let abortController = new AbortController();
-      let signal = abortController.signal;
-
-        setLoading(true);
-        fetch(url , {
-          signal
-        })
-        .then(res => {
-          if(!res.ok) {
-            throw Error('something went wrong');
-          }
-          return res.json();
-        })
-        .then(data => {
-          setData(data);
-          setError(null);
-          setLoading(false);
-        })
-        .catch(e => {
-          setError(e.message);
-        })
-
-        //cleanup function
-        return () => {
-          abortController.abort();
-        }
-      },[url]);
-    return { data , loading , error };
-}
-
-export default useFetch;
->>>>>>> 40e7b82d390ad94adf1ee9d0ecc5f95d4cd861c3

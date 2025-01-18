f5517913defdb11dd0f411ae1951563bc6f54a61
Hacker Mg Pyuue <phyozawlinn1852020@gmail.com>
2025-01-18 11:50:37 +0630

    first commit

---
+++ MERN/frontend/mern-class/src/main.jsx
@@ -0,0 +1,42 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+
+import About from './pages/About.jsx';
+import Contact from './pages/Contact.jsx';
+import Home from './pages/Home.jsx';
+
+import App from './App.jsx';
+
+import {
+  RouterProvider,
+  createBrowserRouter
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />
+      },
+      {
+        path: "/about",
+        element: <About />
+      },
+      {
+        path: "/contact",
+        element: <Contact />
+      }
+    ]
+  }
+]);
+
+const root = createRoot(document.getElementById('root'));
+root.render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
+);

0b1509cb29183003bcdcd26dea48356dc0bcc482
Hacker Mg Pyuue <phyozawlinn1852020@gmail.com>
2025-01-20 22:08:00 +0630

    ch

--- MERN/frontend/mern-class/src/pages/RecipeForm.jsx
+++ MERN/frontend/mern-class/src/pages/RecipeForm.jsx
@@ -1,22 +1,50 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useState } from 'react'
 import plus from '../assets/plus.svg'
 import Ingredients from '../components/ingredients'
 
 export default function RecipeForm() {
+
+  let [ingredients,setIngredients] = useState([])
+  let [newingredients,setNewIngredients] = useState('')
+  let [title,setTitle] = useState('')
+  let [description,setDescription] = useState('')
+  let [errors,setErrors] = useState([])
+
+  let addIngredients = () => {
+     setIngredients(prev => [newingredients,...prev])
+     setNewIngredients('')
+  }
+
+  let createRecipe = async (e) => {
+    try {
+      e.preventDefault()
+      let recipe = {
+        title,
+        description,
+        ingredients
+      } 
+      let res = await axios.post('http://localhost:4000/api/recipes', recipe)
+      console.log(res)
+    } catch(e) {
+      setErrors(e.response.data.errors)
+    }
+  }
+
   return (
     <div className='mx-auto max-w-md border-2 border-white p-4'>
         <h1 className='mb-6 text-2xl font-bold text-orange-500 text-center'>Recipe Create Form</h1>
-        <form action="" className='space-y-5'>
-            <input type="text" placeholder='Recipe Title' className='w-full p-1'/>
-            <textarea name="" id="" placeholder='Recipe Description' className='w-full p-1' rows="5"></textarea>
+        <form action="" className='space-y-5' onSubmit={createRecipe}>
+            <input value={title} onChange={e => setTitle(e.target.value)} type="text" placeholder='Recipe Title' className='w-full p-1'/>
+            <textarea value={description} onChange={e => setDescription(e.target.value)} name="" id="" placeholder='Recipe Description' className='w-full p-1' rows="5"></textarea>
             <div className='flex space-x-2 items-center'>
-              <input type="text" placeholder='Recipe Ingredients' className='w-full p-1'/>
-              <img src={plus} alt="" className='w-6 h-6 cursor-pointer' />
+              <input onChange={e => setNewIngredients(e.target.value)} type="text" placeholder='Recipe Ingredients' className='w-full p-1' value={newingredients}/>
+              <img onClick={addIngredients} src={plus} alt="" className='w-6 h-6 cursor-pointer' />
             </div>
             <div>
-              <Ingredients ingredients={[]}/>
+              <Ingredients ingredients={ingredients}/>
             </div>
-            <button className='w-full px-3 py-1 rounded-xl bg-orange-500'>Create Recipe</button>
+            <button type='submit' className='w-full px-3 py-1 rounded-xl border-2 bg-orange-500 hover:bg-white hover:text-orange-500 hover:border-orange-500 b- transaction duration-300 ease-in-out'>Create Recipe</button>
         </form>
     </div>
   )
